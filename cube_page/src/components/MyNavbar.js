c711290dac022e863643f56852b71ecc8dd34169
[madhan2023] <[madhankumarjoth@gmail.com]>
2024-03-03 20:07:22 +0530

    add menu list

--- cube_page/src/components/MyNavbar.js
+++ cube_page/src/components/MyNavbar.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import Logo from '../assets/images/logo-alt.png'
 
 const MyNavbar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar sticky="top" bg="primary" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#">My Website</Navbar.Brand>
+        <Navbar.Brand href="#"><img src={Logo} alt='cube logo' /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link className="px-5 text-white" href="#home"><strong>Home</strong></Nav.Link>
+            <Nav.Link className="px-5 text-white" href="#about"><strong>About</strong></Nav.Link>
+            <Nav.Link className="px-5 text-white" href="#service"><strong>Service</strong></Nav.Link>
+            <Nav.Link className="px-5 text-white" href="#portfolio"><strong>Portfolio</strong></Nav.Link>
+            <Nav.Link className="px-5 text-white" href="#about"><strong>Blog</strong></Nav.Link>
+            <Nav.Link className="px-5 text-white" href="#contact"><strong>Contact</strong></Nav.Link>
           </Nav>
-          <Button variant="outline-success">Sign Up</Button>
+          <Button variant="outline-light">Sign Up</Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
